c478c7d587c908f7416a9cb14ce7d31fb370fad3
terry1945 <99671870+terry1945@users.noreply.github.com>
2023-09-05 06:08:51 -0600

    1st commit
    nvim lua files with packer, gruvbox, lualine, treesitter

---
+++ nvim/lua/core/keymaps.lua
@@ -0,0 +1,20 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+--vim.opt.cursorline = true
+vim.opt.autoread = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- use spaces for tabs
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
